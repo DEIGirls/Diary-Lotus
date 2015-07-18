982292dd7383effdde099035eca097337e017b34
Christian Weyer <caducitynights@gmail.com>
2015-07-18 11:47:05 +0200

    Add authentication/login together with @rosafaria

--- apps/web/controllers/entries/authenticate.rb
+++ apps/web/controllers/entries/authenticate.rb
@@ -2,38 +2,22 @@ require 'lotus/action'
 require 'lotus/controller'
 require 'lotus/action/session'
 require 'bcrypt'
-      
+
 module Web::Controllers::Entries
   class Authenticate
     include Web::Action
-	include Lotus::Action::Session
-	
-	expose :user
-	
+    include Lotus::Action::Session
+
     def call(params)
+      email = params[:user].fetch("email")
+      user  = UserRepository.user_by_email(email)
 
-    	users = UserRepository.all
-    	users.each do |e|
-    		if (e.email == params[:user].fetch("email")) 
-    		then
-    			@user = e
-    		end
-    	end
-
-    	if @user == nil 
-    		then redirect_to '/entries'
+      if user && BCrypt::Password.new(user.password) == params[:user].fetch("password")
+        session[:user_id] = user.id
+        redirect_to '/entries'
+    	else
+        redirect_to '/entries/login'
+      end
 		end
-		
-		aux = BCrypt::Password.new( @user.password )
-
-		puts params[:user].fetch("password")
-
-		if params[:user].fetch("password").chomp== aux
-			puts 'comé?'
-			session[:user_id] = @user.id
-		end
-
-		redirect_to '/entries'
-    end
   end
 end
--- lib/diary/repositories/user_repository.rb
+++ lib/diary/repositories/user_repository.rb
@@ -1,3 +1,11 @@
 class UserRepository
   include Lotus::Repository
+
+  class << self
+    def user_by_email(email)
+      query do
+        where(email: email)
+      end.first
+    end
+  end
 end
